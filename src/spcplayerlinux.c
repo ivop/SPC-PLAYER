c7e659e1dfcf6de4cbb7ec2e8a2a2f7f25fbf760
Ivo van Poorten <ivopvp@gmail.com>
2023-08-08 02:42:21 +0200

    clean up pass 1. test for errors, write errors to stderr, proper return from function.

--- src/spcplayerlinux.c
+++ src/spcplayerlinux.c
@@ -4,20 +4,13 @@
  * Version 0.2
  */
 
-#ifdef __linux__
-#    include <stdio.h>
-#    include <stdlib.h>
-#    include <sys/ioctl.h>
-#    include <sys/types.h>
-#    include <sys/mman.h>
-#    include <sys/soundcard.h>
-#    include <fcntl.h>
-#    include "openspc.h"
-#    include <unistd.h>
-#    include <sys/stat.h>
-#    include <ctype.h>
-#    include <string.h>
-#endif
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/soundcard.h>
+#include "openspc.h"
 
 int main(int argc, char *argv[]) {
     int optionoffset = 0, audio_fd, channels = 2, rformat, rchannels, format =
@@ -30,10 +23,10 @@ int main(int argc, char *argv[]) {
 
 
     if ((argc < 2)) {
-        printf
-            ("spcplayer version %s\nusage: spcplayer [sound device] filename.spc\n",
+        printf("spcplayer version %s\n"
+                "usage: spcplayer [sound device] filename.spc\n",
              ver);
-        exit(1);
+        return 1;
     }
 
     if ((argc > 2)) {
@@ -42,36 +35,36 @@ int main(int argc, char *argv[]) {
     }
 
     if ((audio_fd = open(audio_device, O_WRONLY, 0)) == -1) {
-        printf("[-] Could not open, %s.\n", audio_device);
-        exit(1);
+        fprintf(stderr, "[-] Could not open, %s.\n", audio_device);
+        return 1;
     }
     printf("[+] Successfully opened, %s.\n", audio_device);
 
     rformat = format;
     if (ioctl(audio_fd, SNDCTL_DSP_SETFMT, &format) == -1) {
-        printf("[-] Could not set sound format.\n");
-        exit(1);
+        fprintf(stderr, "[-] Could not set sound format.\n");
+        return 1;
     }
     if (format != rformat) {
-        printf("[-] Could not set sound format.\n");
-        exit(1);
+        fprintf(stderr, "[-] Could not set sound format.\n");
+        return 1;
     }
     printf("[+] Successfully set sound format.\n");
 
     rchannels = channels;
     if (ioctl(audio_fd, SNDCTL_DSP_CHANNELS, &channels) == -1) {
-        printf("[-] Could not set channels.\n");
-        exit(1);
+        fprintf(stderr, "[-] Could not set channels.\n");
+        return 1;
     }
     if (channels != rchannels) {
-        printf("[-] Could not set channels.\n");
-        exit(1);
+        fprintf(stderr, "[-] Could not set channels.\n");
+        return 1;
     }
     printf("[+] Successfully set channels.\n");
 
     if (ioctl(audio_fd, SNDCTL_DSP_SPEED, &speed) == -1) {
-        printf("[-] Could not set speed.\n");
-        exit(1);
+        fprintf(stderr, "[-] Could not set speed.\n");
+        return 1;
     }
     printf("[+] Using speed, {%iHz}\n", speed);
 
@@ -79,14 +72,18 @@ int main(int argc, char *argv[]) {
 
     fd = open(argv[1 + optionoffset], O_RDONLY);
     if (fd < 0) {
-        printf("[-] Could not open \'%s\'\n.", argv[1]);
-        exit(1);
+        fprintf(stderr, "[-] Could not open \'%s\'\n.", argv[1]);
+        return 1;
     }
 
     size = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
     ptr = malloc(size);
-    read(fd, ptr, size);
+    int ret = read(fd, ptr, size);
+    if (ret <0) {
+        fprintf(stderr, "error reading file\n");
+        return 1;
+    }
     close(fd);
 
     fd = OSPC_Init(ptr, size);
@@ -97,7 +94,11 @@ int main(int argc, char *argv[]) {
 
     while ((read(STDIN_FILENO, &c, 1)) <= 0) {
         size = OSPC_Run(-1, buf, 32000);
-        write(audio_fd, buf, size);
+        int ret = write(audio_fd, buf, size);
+        if (ret < 0) {
+            fprintf(stderr, "error writing to audio device\n");
+            return 1;
+        }
     }
 
     printf("\nGoodbye!\n");
